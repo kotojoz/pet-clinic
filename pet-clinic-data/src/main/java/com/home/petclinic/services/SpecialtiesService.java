a339b65fa9649a0471b704dd904873bb64b5668a
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2021-12-09 17:33:06 +0200

    Added Specialties Map service Impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/home/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.home.petclinic.services;
+
+import com.home.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
--- pet-clinic-data/src/main/java/com/home/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/home/petclinic/services/map/PetServiceMap.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 @Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
