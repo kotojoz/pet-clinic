ad72a12f1ca073371d6c3719f2ca8bc064cd29ee
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2021-12-09 15:16:09 +0200

    added PetTypes to bootstrap. closes #32.

--- pet-clinic-web/src/main/java/com/home/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/home/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.home.petclinic.bootstrap;
 
 import com.home.petclinic.model.Owner;
+import com.home.petclinic.model.Pet;
 import com.home.petclinic.model.PetType;
 import com.home.petclinic.model.Vet;
 import com.home.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.home.petclinic.services.PetTypeService;
 import com.home.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123123123");
 
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glennane");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123123123");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
