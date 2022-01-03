ef242162aa9b9b3599a11ab34ebd0b7aff29646c
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-01-03 22:27:15 +0200

    impl find owner. Closes #54.

--- pet-clinic-data/src/main/java/com/home/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/home/petclinic/services/OwnerService.java
@@ -1,9 +1,11 @@
 package com.home.petclinic.services;
 import com.home.petclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
